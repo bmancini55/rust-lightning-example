b9463a7f47976567132fb3d9a3993daf6720a31f
Brian Mancini <bmancini@gmail.com>
2020-08-30 14:44:11 -0400

    refactor connect_to_node to method

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use bitcoin::network::constants::Network;
-use bitcoin::secp256k1::key::SecretKey;
-use std::sync::Arc;
+use bitcoin::secp256k1::key::{PublicKey, SecretKey};
+use std::net::SocketAddr;
+use tokio;
 mod chain;
 mod log;
 
@@ -13,14 +14,20 @@ async fn main() {
 
     let user_config = lightning::util::config::UserConfig::default();
 
-    let democlient = Arc::new(client::LightingClient::new(
-        node_key,
-        &seed,
-        user_config,
-        Network::Testnet,
-    ));
+    let demo_client = client::LightingClient::new(node_key, &seed, user_config, Network::Testnet);
 
-    client::connect(democlient).await;
+    // demo1.lndexplorer.com
+    let node_id_str = "036b96e4713c5f84dcb8030592e1bd42a2d9a43d91fa2e535b9bfd05f2c5def9b9";
+    let node_addr_str = "38.87.54.163:9745";
+
+    // // eclair
+    // let node_id_str = "03933884aaf1d6b108397e5efe5c86bcf2d8ca8d2f700eda99db9214fc2712b134";
+    // let node_addr_str = "34.250.234.192:9735";
+
+    let node_id_slice = &hex::decode(node_id_str).unwrap();
+    let node_id = PublicKey::from_slice(&node_id_slice).unwrap();
+    let node_addr: SocketAddr = node_addr_str.parse().unwrap();
+    demo_client.connect_to_node(node_id, node_addr).await;
 }
 
 mod client {
@@ -28,14 +35,45 @@ mod client {
     use crate::log;
     use bitcoin::network::constants::Network;
     use bitcoin::secp256k1::key::{PublicKey, SecretKey};
-    use hex;
     use lightning::chain::keysinterface::KeysManager;
     use lightning::util::events::EventsProvider;
-    use lightning::util::logger::Logger as LoggerTrait;
-    use lightning::util::logger::{Level, Record};
     use std::net::SocketAddr;
     use std::sync::Arc;
     use tokio::sync::mpsc;
+
+    // Define concrete types for our high-level objects:
+    type TxBroadcaster = dyn lightning::chain::chaininterface::BroadcasterInterface;
+    type FeeEstimator = dyn lightning::chain::chaininterface::FeeEstimator;
+    type ChainWatchInterface = dyn lightning::chain::chaininterface::ChainWatchInterface;
+    type Logger = dyn lightning::util::logger::Logger;
+    type ChannelMonitor = lightning::ln::channelmonitor::SimpleManyChannelMonitor<
+        lightning::chain::transaction::OutPoint,
+        lightning::chain::keysinterface::InMemoryChannelKeys,
+        Arc<TxBroadcaster>,
+        Arc<FeeEstimator>,
+        Arc<Logger>,
+        Arc<ChainWatchInterface>,
+    >;
+    type ChannelManager = lightning::ln::channelmanager::SimpleArcChannelManager<
+        ChannelMonitor,
+        TxBroadcaster,
+        FeeEstimator,
+        Logger,
+    >;
+    type NetGraphManager = Arc<
+        lightning::routing::network_graph::NetGraphMsgHandler<
+            Arc<ChainWatchInterface>,
+            Arc<Logger>,
+        >,
+    >;
+    type PeerManager = lightning::ln::peer_handler::SimpleArcPeerManager<
+        lightning_net_tokio::SocketDescriptor,
+        ChannelMonitor,
+        TxBroadcaster,
+        FeeEstimator,
+        ChainWatchInterface,
+        Logger,
+    >;
 
     /// Super basic wrapper class that is going to implement the bare
     /// minimum needed so we can look for p2p messaging traffic and
@@ -140,196 +178,36 @@ mod client {
         }
     }
 
-    pub async fn connect(client: Arc<LightingClient>) {
-        let node_id = PublicKey::from_slice(
-            &hex::decode("036b96e4713c5f84dcb8030592e1bd42a2d9a43d91fa2e535b9bfd05f2c5def9b9")
-                .unwrap(),
-        )
-        .unwrap();
-        let node_addr: SocketAddr = "38.87.54.163:9745".parse().unwrap();
-        // let node_id = PublicKey::from_slice(
-        //     &hex::decode("03933884aaf1d6b108397e5efe5c86bcf2d8ca8d2f700eda99db9214fc2712b134").unwrap(),
-        // )
-        // .unwrap();
-        // let node_addr: SocketAddr = "34.250.234.192:9735".parse().unwrap();
-        println!("{:?}", node_id);
-        println!("{:?}", node_addr);
-        let logger = Arc::new(log::ConsoleLogger::new());
-        Arc::clone(&logger).log(&Record::new(
-            Level::Info,
-            format_args!("{}", "hello"),
-            "module",
-            "file",
-            22,
-        ));
+    impl LightingClient {
+        // Connect to node with pubkey their_node_id at addr:
+        pub async fn connect_to_node(&self, their_node_id: PublicKey, addr: SocketAddr) {
+            let (sender, mut receiver) = mpsc::channel(2);
 
-        // let bitcoin_client = Arc::new(chain::FakeBitcoinClient::new(logger.clone()));
-        // let tx = bitcoin::blockdata::transaction::Transaction {
-        //     version: 2,
-        //     lock_time: 0,
-        //     input: vec![],
-        //     output: vec![],
-        // };
-        // bitcoin_client.broadcast_transaction(&tx);
+            lightning_net_tokio::connect_outbound(
+                self.peer_manager.clone(),
+                sender,
+                their_node_id,
+                addr,
+            )
+            .await;
 
-        // let channel_monitor = Arc::new(ChannelMonitor::new(
-        //     bitcoin_client.clone(),
-        //     bitcoin_client.clone(),
-        //     logger.clone(),
-        //     bitcoin_client.clone(),
-        // ));
-
-        // let secp_context = bitcoin::secp256k1::Secp256k1::new();
-        // let funding_key = bitcoin::secp256k1::SecretKey::from_slice(&[0x01; 32]).unwrap();
-        // let revocation_base_key = bitcoin::secp256k1::SecretKey::from_slice(&[0x02; 32]).unwrap();
-        // let payment_key = bitcoin::secp256k1::SecretKey::from_slice(&[0x03; 32]).unwrap();
-        // let delayed_payment_key = bitcoin::secp256k1::SecretKey::from_slice(&[0x04; 32]).unwrap();
-        // let htlc_key = bitcoin::secp256k1::SecretKey::from_slice(&[0x5; 32]).unwrap();
-        // let keys_manager = Arc::new(InMemoryChannelKeys::new(
-        //     &secp_context,
-        //     funding_key,
-        //     revocation_base_key,
-        //     payment_key,
-        //     delayed_payment_key,
-        //     htlc_key,
-        //     [0x00; 32],
-        //     0_u64,
-        //     (0_u64, 0_u64),
-        // ));
-
-        // let ts = std::time::SystemTime::now()
-        //     .duration_since(std::time::UNIX_EPOCH)
-        //     .unwrap();
-        // let seed = rand::random::<[u8; 32]>();
-        // let keys_manager = Arc::new(KeysManager::new(
-        //     &seed,
-        //     Network::Testnet,
-        //     ts.as_secs(),
-        //     ts.subsec_nanos(),
-        // ));
-
-        // let user_config = lightning::util::config::UserConfig::default();
-        // let channel_manager: ChannelManager =
-        //     Arc::new(lightning::ln::channelmanager::ChannelManager::new(
-        //         Network::Testnet,
-        //         bitcoin_client.clone(),
-        //         channel_monitor.clone(),
-        //         bitcoin_client.clone(),
-        //         logger.clone(),
-        //         keys_manager,
-        //         user_config,
-        //         1000000,
-        //     ));
-
-        // // next construct the network graph manager. This imple
-        // let net_graph_manager: NetGraphManager =
-        //     Arc::new(lightning::routing::network_graph::NetGraphMsgHandler::new(
-        //         bitcoin_client.clone(),
-        //         logger.clone(),
-        //     ));
-
-        // // now that we have implemented the two types for message
-        // // handling we can construct a MessageHandler which contains
-        // // references to the different message handlers and it is
-        // // used by the peer
-        // let message_handler = lightning::ln::peer_handler::MessageHandler {
-        //     chan_handler: channel_manager.clone(),
-        //     route_handler: net_graph_manager.clone(),
-        // };
-        // let node_key = bitcoin::secp256k1::SecretKey::from_slice(
-        //     &hex::decode("d84985781fee4676a616f81399d28cced95a691a983c582b6285108e02830673")
-        //         .unwrap(),
-        // )
-        // .unwrap();
-        // let peer_manager: PeerManager = Arc::new(lightning::ln::peer_handler::PeerManager::new(
-        //     message_handler,
-        //     node_key,
-        //     &rand::random::<[u8; 32]>(),
-        //     logger.clone(),
-        // ));
-
-        connect_to_node(
-            client.peer_manager.clone(),
-            client.channel_monitor.clone(),
-            client.channel_manager.clone(),
-            node_id,
-            node_addr,
-        )
-        .await;
-    }
-
-    // Define concrete types for our high-level objects:
-    type TxBroadcaster = dyn lightning::chain::chaininterface::BroadcasterInterface;
-    type FeeEstimator = dyn lightning::chain::chaininterface::FeeEstimator;
-    type ChainWatchInterface = dyn lightning::chain::chaininterface::ChainWatchInterface;
-    type Logger = dyn lightning::util::logger::Logger;
-    type ChannelMonitor = lightning::ln::channelmonitor::SimpleManyChannelMonitor<
-        lightning::chain::transaction::OutPoint,
-        lightning::chain::keysinterface::InMemoryChannelKeys,
-        Arc<TxBroadcaster>,
-        Arc<FeeEstimator>,
-        Arc<Logger>,
-        Arc<ChainWatchInterface>,
-    >;
-    type ChannelManager = lightning::ln::channelmanager::SimpleArcChannelManager<
-        ChannelMonitor,
-        TxBroadcaster,
-        FeeEstimator,
-        Logger,
-    >;
-    type NetGraphManager = Arc<
-        lightning::routing::network_graph::NetGraphMsgHandler<
-            Arc<ChainWatchInterface>,
-            Arc<Logger>,
-        >,
-    >;
-    type PeerManager = lightning::ln::peer_handler::SimpleArcPeerManager<
-        lightning_net_tokio::SocketDescriptor,
-        ChannelMonitor,
-        TxBroadcaster,
-        FeeEstimator,
-        ChainWatchInterface,
-        Logger,
-    >;
-
-    // Connect to node with pubkey their_node_id at addr:
-    async fn connect_to_node(
-        peer_manager: PeerManager,
-        channel_monitor: Arc<ChannelMonitor>,
-        channel_manager: ChannelManager,
-        their_node_id: PublicKey,
-        addr: SocketAddr,
-    ) {
-        let (sender, mut receiver) = mpsc::channel(2);
-        lightning_net_tokio::connect_outbound(peer_manager, sender, their_node_id, addr).await;
-        loop {
-            receiver.recv().await;
-            for _event in channel_manager.get_and_clear_pending_events().drain(..) {
-                // Handle the event
-            }
-            for _event in channel_monitor.get_and_clear_pending_events().drain(..) {
-                // Handle the event
+            loop {
+                receiver.recv().await;
+                for _event in self
+                    .channel_manager
+                    .get_and_clear_pending_events()
+                    .drain(..)
+                {
+                    // Handle the event
+                }
+                for _event in self
+                    .channel_monitor
+                    .get_and_clear_pending_events()
+                    .drain(..)
+                {
+                    // Handle the event
+                }
             }
         }
     }
-
-    // Begin reading from a newly accepted socket and talk to the peer:
-    // async fn accept_socket(
-    //     peer_manager: PeerManager,
-    //     channel_monitor: Arc<ChannelMonitor>,
-    //     channel_manager: ChannelManager,
-    //     socket: TcpStream,
-    // ) {
-    //     let (sender, mut receiver) = mpsc::channel(2);
-    //     lightning_net_tokio::setup_inbound(peer_manager, sender, socket);
-    //     loop {
-    //         receiver.recv().await;
-    //         for _event in channel_manager.get_and_clear_pending_events().drain(..) {
-    //             // Handle the event
-    //         }
-    //         for _event in channel_monitor.get_and_clear_pending_events().drain(..) {
-    //             // Handle the event
-    //         }
-    //     }
-    // }
 }
